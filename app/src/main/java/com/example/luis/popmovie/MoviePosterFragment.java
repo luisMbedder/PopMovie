9b544ffab85233facb59ec3175d69085ddcc5648
Luis Marquez <gunclub84@gmail.com>
2016-02-02 22:00:01 -0700

    added MovieDetailActivity and fragment

--- app/src/main/java/com/example/luis/popmovie/MoviePosterFragment.java
+++ app/src/main/java/com/example/luis/popmovie/MoviePosterFragment.java
@@ -1,10 +1,13 @@
 package com.example.luis.popmovie;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
 
@@ -13,7 +16,7 @@ import android.widget.GridView;
  */
 public class MoviePosterFragment extends Fragment {
 
-    GridView gridView;
+    GridView movieGrid;
 
     static final String[] numbers = new String[] {
             "A", "B", "C", "D", "E",
@@ -28,15 +31,35 @@ public class MoviePosterFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
+        //inflate view object
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-
-        gridView = (GridView)rootView.findViewById(R.id.movieGridView);
+        //get gridView
+        movieGrid = (GridView)rootView.findViewById(R.id.movieGridView);
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
                 android.R.layout.simple_list_item_1, numbers);
 
-        gridView.setAdapter(adapter);
+         movieGrid.setAdapter(adapter);
+        movieGrid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick (AdapterView<?> parent, View view, int position, long id)
+            {
+                //get activity the fragment is associated with
+                Context context = getActivity();
+                //parent:The adapterview where the click happened
+                Object textObject = parent.getItemAtPosition(position);
+                String forecast = textObject.toString();
+                //starting a new activity is packaged as an intent
+                Intent intent = new Intent(context, MovieDetailActivity.class);
+                //intent.putExtra(EXTRA_MESSAGE, forecast);
+                //start detail Activity
+                startActivity(intent);
+            }
+
+        });
+
+
 
         return rootView;
 
